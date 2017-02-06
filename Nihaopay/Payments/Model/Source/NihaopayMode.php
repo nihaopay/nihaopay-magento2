da4f8d902125f519be55936ea255378229a0dd58
chengzhang <676931854@qq.com>
2017-02-06 18:13:24 +0800

    change files

---
+++ Nihaopay/Payments/Model/Source/NihaopayMode.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Nihaopay\Payments\Model\Source;
+
+use Magento\Framework\Option\ArrayInterface;
+
+
+class WorldpayMode implements ArrayInterface {
+
+	public function toOptionArray() {
+		return [
+			['value' => 'test_mode', 'label' =>__('Test Mode')],
+			['value' => 'live_mode', 'label' => __('Live Mode')]
+			];
+	}
+
+}
