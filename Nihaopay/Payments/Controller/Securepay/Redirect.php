8c5a44d8bb860035a0d8c52eefcebaefae5f9de4
chengzhang <676931854@qq.com>
2017-02-09 10:31:02 +0800

    change files

--- Nihaopay/Payments/Controller/Securepay/Redirect.php
+++ Nihaopay/Payments/Controller/Securepay/Redirect.php
@@ -29,22 +29,22 @@ class Redirect extends Apm
             ]);
         }
 
-        // try {
-        //     $order = $this->methods[$code]->createMagentoOrder($quote);
-        // }
-        // catch(\Exception $e) {
-        //     return $result->setData([
-        //         'success' => false,
-        //         'error' => $e->getMessage()
-        //     ]);
-        // }
+        try {
+            $order = $this->methods[$code]->createMagentoOrder($quote);
+        }
+        catch(\Exception $e) {
+            return $result->setData([
+                'success' => false,
+                'error' => $e->getMessage()
+            ]);
+        }
 
-        // if (!$order) {
-        //     return $result->setData([
-        //         'success' => false,
-        //         'error' => 'Error, please try again'
-        //     ]);
-        // }
+        if (!$order) {
+            return $result->setData([
+                'success' => false,
+                'error' => 'Error, please try again'
+            ]);
+        }
 
         // $order->addStatusHistoryComment(
         //     __('Redirecting user with Nihaopay Order Code  #%1.', $reference)
