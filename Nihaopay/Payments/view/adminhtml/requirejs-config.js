1851d041ca428d25e2d08c35007c12d56d550b8b
chengzhang <676931854@qq.com>
2017-02-09 15:50:54 +0800

    change files

--- Nihaopay/Payments/view/adminhtml/requirejs-config.js
+++
@@ -1,8 +0,0 @@
-var config = {
-    map: {
-        '*': {
-            worldpay: 'https://cdn.worldpay.com/v1/worldpay.js',
-            worldpayForm: 'Nihaopay_Payments/js/wp-form',
-        }
-    }
-};
--- Nihaopay/Payments/view/frontend/requirejs-config.js
+++
@@ -1,7 +0,0 @@
-var config = {
-    map: {
-        '*': {
-            worldpay: 'https://cdn.worldpay.com/v1/worldpay.js',
-        }
-    }
-};
