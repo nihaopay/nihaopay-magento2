36cd59ae82ebcaa22ae9dfd11af7de2f592ec33f
chengzhang <676931854@qq.com>
2017-02-09 16:02:28 +0800

    change files

--- Nihaopay/Payments/view/frontend/web/js/view/payment/method-renderer.js
+++ Nihaopay/Payments/view/frontend/web/js/view/payment/method-renderer.js
@@ -9,11 +9,8 @@ define(
               rendererList) {
         'use strict';
 
-        window.WorldpayMagentoVersion = '2.0.25';
-        var defaultComponent = 'Nihaopay_Payments/js/view/payment/method-renderer/card';
         var apmComponent = 'Nihaopay_Payments/js/view/payment/method-renderer/apm';
-        var giropayComponent = 'Nihaopay_Payments/js/view/payment/method-renderer/giropay';
-        var idealComponent = 'Nihaopay_Payments/js/view/payment/method-renderer/ideal';
+     
 
         var methods = [
              {type: 'nihaopay_payments_alipay', component: apmComponent},
